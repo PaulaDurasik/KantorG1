cca5fbbb44abff276a44639f1da4484e6aa7fa95
Paula Durasik <paula.durasik@gmail.com>
2017-11-26 10:56:54 +0100

    for

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Menu.getInstance().Display(Login);
+        Menu.getInstance().display(LOGIN);
     }
 }
---
+++ src/main/java/Menu.java
@@ -0,0 +1,19 @@
+public class Menu {
+    public static Menu instance=null;
+
+
+    public static Menu getInstance(){
+        if (instance==null){
+            instance=new Menu();
+        }
+        return instance;
+
+    }
+
+    public void display(){
+        System.out.println("Login");
+
+    }
+
+
+}
