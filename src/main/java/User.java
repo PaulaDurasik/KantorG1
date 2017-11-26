9b7f2b390f0759f4b542c1689f8ec97b19f1ae8c
Ilona Solarek <ilona.solarek@gmail.com>
2017-11-26 11:11:30 +0100

    second

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,4 +5,22 @@ public class User {
     String pass;
 
     Role role;
+
+    public User(String login, String pass, Role role) {
+        this.login = login;
+        this.pass = pass;
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
